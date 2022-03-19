6d582d1ae2d19ed3984001c244f48541a54a861b
revue-starlight <204214884@qq.com>
2022-03-19 04:09:45 +0000

    upd

--- ree/src/mail/mail.dto.ts
+++ ree/src/mail/mail.dto.ts
@@ -1,21 +1,21 @@
-import { Mail, EmailRequest,MailUrl } from "../../protocols/email"
+import { Mail, MailRequest,MailUrl } from "../../protocols/email"
 import { IsEmail, IsNumberString } from 'class-validator'
-export class MailRequestDto implements EmailRequest {
+export class MailRequestDto implements MailRequest {
     @IsEmail()
     email: MailUrl;
 }
 
-export class ModifyEmailRequestDto implements EmailRequest {
+export class ModifyEmailRequestDto implements MailRequest {
     @IsEmail()
     email: MailUrl;
 }
 
-export class DeleteEmailRequestDto implements EmailRequest {
+export class DeleteEmailRequestDto implements MailRequest {
     @IsNumberString()
     emailId: string;
 }
 
-export class AddEmailRequestDto implements EmailRequest{
+export class AddEmailRequestDto implements MailRequest{
     @IsEmail()
     email: MailUrl;
 
